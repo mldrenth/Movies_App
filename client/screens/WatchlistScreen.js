212ecfa9047bf297736e8e12a3c969eef8f32735
Marijke Drenth <drenth.marijke@googlemail.com>
2022-02-22 12:08:00 +0200

    Add navigator

---
+++ client/screens/WatchlistScreen.js
@@ -0,0 +1,13 @@
+import {React, useState, useEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView} from 'react-native';
+
+const WatchlistScreen = () => {
+
+    return (
+        <Text> WatchlistScreen</Text>
+    )
+    
+}
+
+export default WatchlistScreen;
