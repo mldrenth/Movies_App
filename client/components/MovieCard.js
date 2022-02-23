3911ff52bedff3fa32ac4f8c7cc3dac5d2709bb9
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-02-23 18:00:50 +0000

    Movie card features added

--- client/components/MovieCard.js
+++ client/components/MovieCard.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import {Text, View, Image, StyleSheet} from 'react-native';
+import { FlatList } from 'react-native-web';
 
 const MovieCard = ({id, backdropPath, posterPath, genreIds, title, overview, voteAverage, releaseDate, popularity, userRating}) => {
 
     const baseUrl = {uri: "https://image.tmdb.org/t/p/w500/" + posterPath};
 
     return (
-        <View>
-            <Image source={baseUrl} style={styles.image}></Image>
+        <View style={{flexDirection: 'row', width: 160, padding: 10, backgroundColor: 'aquamarine'}}>
             <View>
-                <Text>{title}</Text>
+            <Image source={baseUrl} style={styles.image}></Image>
+            </View>
+            <View style={{margin: 10}}>
+                <Text style={{fontSize:20}}>{title}</Text>
+                <Text style={{fontSize:20, marginBottom: 10}}>({releaseDate.substring(0, 4)})</Text>
+                <Text style={{marginBottom: 10}}>{overview.substring(0, 100) + "..."}</Text>
+                <Text>{voteAverage} {userRating}</Text>
             </View>
         </View>
     )
@@ -17,17 +23,12 @@ const MovieCard = ({id, backdropPath, posterPath, genreIds, title, overview, vot
 }
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 0,
-        marginVertical: 8,
-        marginHorizontal: 0,
-        
-      },image: {
+    image: {
         flex: 1,
         resizeMode: 'contain',
-        height: 100,
-        width: 200
-    
+        height: 300,
+        width: 200,
+        borderRadius: 10
       }
 
 });
