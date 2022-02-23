04bf5baaaa0feeac493de4e1c86805a97a21d21d
Marijke Drenth <drenth.marijke@googlemail.com>
2022-02-23 19:35:33 +0200

    Add basic saving functionality

--- client/services/UserServices.js
+++ client/services/UserServices.js
@@ -6,3 +6,25 @@ export const getUserData = () => {
     return fetch(baseURL + "/1")
     .then((res) => res.json())
 }
+
+export const addMovieToFavourites = (movie) => {
+    return fetch(baseURL + "/1/favourites", {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+}
+
+export const addMovieToWatchlist = (movie) => {
+    return fetch(baseURL + "/1/watchlist", {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+}
