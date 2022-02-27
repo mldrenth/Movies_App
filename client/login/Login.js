effb6b200d48a4b7e20bfc76a79c28d54c5c314d
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-02-27 12:56:47 +0000

    Login front end page

--- client/login/Login.js
+++ client/login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState }from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+
  
 const LoginScreen = () => {
 
@@ -12,11 +14,8 @@ const LoginScreen = () => {
         <View style={styles.container}>
             <Image style={styles.image} source = {require("../assets/mt_logo.png")}/>
             <Text style={{color:"#f5c517", fontWeight:"bold", fontSize:20, marginBottom: 30}}>Movie Time</Text>
-
-            <Text style={{color:"white", fontWeight:"bold", marginBottom: 10}}>Sign in</Text>
             <StatusBar style="auto" />
         
-
             <View style={styles.inputView}>
                 <TextInput
                     style={styles.textInput}
@@ -36,36 +35,44 @@ const LoginScreen = () => {
                 />
             </View>
 
-        </View>
+            <TouchableOpacity style={styles.loginBtn}>
+                <AntDesign name="login" size={30} color="#f5c517" />
+                <Text style={{color:"#f5c517", paddingTop:5}}>LOGIN</Text>
+            </TouchableOpacity>
 
+        </View>
     );
 }
  
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-      height: 60,
-      width: 80,
-      marginBottom: 5
-  },
-  textInput: {
-    backgroundColor: "#151d30",
-    borderRadius: 7,
-    borderWidth: 1,
-    borderColor: "#f5c517",
-    width: 230,
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-    textAlign: "center",
-    height: 40,
-    fontSize: 16,
-    color: "white"
-  }
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image: {
+        height: 60,
+        width: 80,
+        marginBottom: 5
+    },
+    textInput: {
+        backgroundColor: "#151d30",
+        borderRadius: 7,
+        borderWidth: 1,
+        borderColor: "#f5c517",
+        width: 230,
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+        textAlign: "center",
+        height: 40,
+        fontSize: 16,
+        color: "white"
+    },
+    loginBtn: {
+        margin: 10,
+        alignItems:"center",
+    }
 });
 
 export default LoginScreen;
--- server/src/main/java/com/codeclan/example/server/repositories/UserRepository.java
+++ server/src/main/java/com/codeclan/example/server/repositories/UserRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User>
 
 }
